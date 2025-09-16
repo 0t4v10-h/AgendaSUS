ad82f1a56749a4c3749dcf7388e2190212e7ce04
Otavio Henrique <otaviooliveirapt@hotmail.com>
2025-09-16 11:59:12 -0300

    Completando o agendamento de consultas

--- AgendaSUS/src/routes/appointment.routes.js
+++ AgendaSUS/src/routes/appointment.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { create, findAll } from "../controllers/appointment.controller.js";
+import { create, findAll, findOne, update, remove } from "../controllers/appointment.controller.js";
 
 const router = Router();
 
 router.post("/", create);
 router.get("/", findAll);
+router.get("/:id", findOne);
+router.put("/:id", update);
+router.delete("/:id", remove);
 
 export default router;
