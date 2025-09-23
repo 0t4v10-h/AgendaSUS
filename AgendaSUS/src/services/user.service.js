9220f8115a53d1508da4ca2f1bad9be9fc98f806
Otavio Henrique <otaviooliveirapt@hotmail.com>
2025-09-23 10:28:11 -0300

    Adicionando validação de CPF, checagem de campos obrigatorios e rejeitando email e CPF duplicados

--- AgendaSUS/src/services/user.service.js
+++ AgendaSUS/src/services/user.service.js
@@ -1,11 +1,32 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
+import { cpf as cpfValidator } from "cpf-cnpj-validator";
 
 const JWT_SECRET = process.env.JWT_SECRET || "secret";
 
 export async function registerUser({ name, email, password, cpf, role }) {
+
+    if (!name || !email || !password || !cpf) {
+        throw new Error("Name, email, senha e CPF são obrigatórios");
+    }
+
+    if (!cpfValidator.isValid(cpf)) {
+        throw new Error("CPF inválido");
+    }
+
+    const existingEmail = await User.findOne({ where: { email } });
+    if (existingEmail) {
+        throw new Error("Email já cadastrado");
+    }
+
+    const existingCpf = await User.findOne({ where: { cpf } });
+    if (existingCpf) {
+        throw new Error("CPF já cadastrado");
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
+
     return await User.create({
         name,
         email,
