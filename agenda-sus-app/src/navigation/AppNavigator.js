dddc8509b09c098a749d0b82fddfb3f6ff8367ff
Otavio Henrique <otaviooliveirapt@hotmail.com>
2025-09-24 15:42:23 -0300

    Retirando a barra superior da pagina inicial

--- agenda-sus-app/src/navigation/AppNavigator.js
+++ agenda-sus-app/src/navigation/AppNavigator.js
@@ -12,7 +12,7 @@ const Stack = createNativeStackNavigator();
 export default function AppNavigator() {
     return (
         <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Register" component={RegisterScreen} />
             <Stack.Screen name="AgendarConsulta" component={AgendarConsultaScreen} />
